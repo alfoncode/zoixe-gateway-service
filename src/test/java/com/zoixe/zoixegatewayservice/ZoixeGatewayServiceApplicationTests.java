64681347cce741c1203c8cc1e8a50416a32e2fc7
alfoncode <ajgarciaba@gmail.com>
2021-02-10 23:12:13 +0100

    first commit

---
+++ src/test/java/com/zoixe/zoixegatewayservice/ZoixeGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoixe.zoixegatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoixeGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
